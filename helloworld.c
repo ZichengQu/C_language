3b5f9d41fc39978a558797d435d56c242344a6df
ZichengQu <quzicheng315@gmail.com>
2021-01-05 17:50:37 +0800

    C build env with VSCode in Linux.

---
+++ helloworld.c
@@ -0,0 +1,18 @@
+// 该程序应用的VSCode插件为C/C++ 和 CMake Tools
+
+#include <stdio.h>
+
+int main(void){
+    int number = 0;
+    while (1) {
+        printf("请输入一个数字："); //Debug运行卡到这里时可能会报错，尽量不要在这里打断点。
+        if (scanf("%d", &number) == 1) {
+            printf("your input is %d\n", number);
+        }
+        if (number == 100) {
+            break;
+        }
+    }
+
+    return 0;
+}
