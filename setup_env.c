05a8657a1f564613d1fafd8989879450dec6fa6f
ZichengQu <quzicheng315@gmail.com>
2021-04-27 10:53:12 +0800

    setup clang-format for VSCode

--- setup_env.c
+++ setup_env.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
-int main(int argc, char** argv){
+int main(int argc, char** argv) {
     // 通过launch.json的args向main函数中传参.
     printf("argc为: %d\n", argc);
-    for(int i=0; i< argc; i++){
+    for(int i = 0; i < argc; i++) {
         printf("argv[%d]为: %s\n", i, argv[i]);
     }
 
     int number = 0;
-    while (1) {
-        printf("请输入一个数字："); //Debug运行卡到这里时可能会报错，尽量不要在这里打断点。
-        if (scanf("%d", &number) == 1) {
+    while(1) {
+        printf("请输入一个数字：");  //Debug运行卡到这里时可能会报错，尽量不要在这里打断点。
+        if(scanf("%d", &number) == 1) {
             printf("your input is %d\n", number);
         }
-        if (number == 100) {
+        if(number == 100) {
             break;
         }
     }
